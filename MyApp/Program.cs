170ed1bd45bd960122955d679efca82677c96783
CoronelExequiel <execoronel14f@gmail.com>
2022-06-03 20:38:47 -0300

    punto 1 corregido

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,61 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace tpn7
+namespace tpn7_Coronel_Francisco_Exequiel
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int marca=0, contador=1;
-            Random rnd = new Random();
+            int marca = 0, contador=1, realizada=0, indice=0, cantidad=0;
+            double tiempo=0;
             List<Tarea> TareasP = new List<Tarea>();
-            List<Tarea> TareasR = new List<Tarea>();
-            Tarea tarea = new Tarea();
+            var TareasR = new List<Tarea>();
+            
+            Random rnd = new Random();
 
-            //Añado tareas a la Lista de tareas pendientes
             do
-            {
-                Console.WriteLine($"Ingrese la descripción de la tarea {contador}: ");
-                tarea.descripcion=Console.ReadLine();
-                tarea.duracion=rnd.Next(10,101);
-                tarea.id=contador;
-                contador=contador+1;
-                TareasP.Add(tarea);
-                Console.WriteLine("Desea ingresar otra tarea?:\n1:SI\n0:NO");
-                marca=Convert.ToInt32(Console.ReadLine());
-            }while (marca!=0);
-
-            marca=0;
-
-            //Mueve las tareas de Lista de Pendientes a Realizadas
-            foreach (Tarea item in TareasP)
-            {
-                Console.WriteLine($"Realizo la tarea? 1:SI, 0:NO\n ID: {item.id}\n Descripción: {item.descripcion}\n Duración: {item.duracion}");
-                marca=Convert.ToInt32(Console.ReadLine());
-                if(marca==1)
+            {   
+                Tarea T = new Tarea();
+                System.Console.WriteLine("Escriba una descripción a su nueva tarea: ");
+                T.Descripcion=Console.ReadLine();
+                T.Id = contador;
+                T.Duracion = rnd.Next(10,100);
+                do
                 {
-                    TareasR.Add(item);
-                    TareasP.Remove(item);
-                }
-            }
-
+                    System.Console.WriteLine("\n¿Desea añadir otra tarea? 1=SI, 0=NO\n");
+                    marca=Convert.ToInt32(Console.ReadLine());
+                }while(marca!=0 && marca!=1);   
+                TareasP.Add(T);
+                contador+=1; 
+            }while(marca!=0);
+            
+            //Muevo las tareas de Pendientes a Realizadas
             foreach(Tarea item in TareasP)
             {
-                Console.WriteLine("Esriba la descripción de la tarea a buscar: ");
-                string aBuscar = Console.ReadLine();
-                if(aBuscar==item.descripcion)
+                Console.WriteLine($"--------------------------\n\tID Tarea: {item.Id} \nDuración: {item.Duracion} \nDescripción: {item.Descripcion}\n--------------------------");
+                do
                 {
-                    Console.WriteLine("Se encontro la tarea!");
+                    Console.WriteLine("Realizo la tarea? 1=SI, 0=NO");
+                    realizada = Convert.ToInt32(Console.ReadLine());
+                }while(realizada!=0 && realizada!=1);
+                if(realizada==1)
+                {
+                    TareasR.Add(item);
                 }
             }
+            foreach(var item in TareasR)
+            {
+                TareasP.Remove(item);
+            }
 
-            int cantHoras=0;
+            //Busco la tarea en pendientes por su descripcion
+            System.Console.WriteLine("\nIngrese la tarea a buscar en pendientes: ");
+            string descripcion = Console.ReadLine();
+            foreach (Tarea item in TareasP)
+            {
+                if(descripcion == item.Descripcion)
+                {
+                    System.Console.WriteLine("Tarea encontrada!");
+                    break;
+                }    
+            }
+
+            //Muestro la cantidad de horas trabajadas 
             foreach(Tarea item in TareasR)
             {
-                cantHoras+=item.duracion;
+                tiempo+=item.Duracion;
             }
-            Console.WriteLine($"Cantidad horas trabajadas: {cantHoras}");
+            tiempo/=60;
+            Console.WriteLine($"Cantidad de horas trabajadas: {tiempo.ToString("N1")}");
+            
+           
+
 
         }
     }
--- MyApp/Tareas.cs
+++ MyApp/Tareas.cs
@@ -1,10 +1,23 @@
 using System;
 
-namespace tpn7
+namespace tpn7_Coronel_Francisco_Exequiel
 {
     public class Tarea
     {
-        public int id, duracion;
-        public string descripcion;
+        //DATOS
+        private int id, duracion;
+        private string descripcion;
+
+        public int Id { get => id; set => id = value; }
+        public int Duracion { get => duracion; set => duracion = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        //CONSTRUCTOR
+        public Tarea()
+        {
+            id = 0;
+            duracion = 0;
+            descripcion = null;
+        }   
     }
 }
